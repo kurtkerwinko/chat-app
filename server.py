b96f20dd15077eedd8fbc528bf17627576f13e10
Kurt Ko <16209032+kurtkerwinko@users.noreply.github.com>
2017-11-26 16:43:16 +0800

    Change conditional for server args

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ SERVER_IP = ''
 SERVER_PORT = 3000
 SERVER_BACKLOG = 5
 
-if (len(sys.argv) == 2):
+if (len(sys.argv) >= 2):
   SERVER_IP = sys.argv[1]
-if (len(sys.argv) == 3):
+if (len(sys.argv) >= 3):
   SERVER_PORT = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
