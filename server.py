2c6710c388cd0ed81fde53f148d4ed32952a75d6
Kurt Ko <16209032+kurtkerwinko@users.noreply.github.com>
2017-11-26 16:41:50 +0800

    Convert port to int

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ SERVER_BACKLOG = 5
 if (len(sys.argv) == 2):
   SERVER_IP = sys.argv[1]
 if (len(sys.argv) == 3):
-  SERVER_PORT = sys.argv[2]
+  SERVER_PORT = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
